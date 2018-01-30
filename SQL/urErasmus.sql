743a71555269a7a1077677b30390ecba8ab8c704
Manuel Vitolo <manuel.vitolo@gmail.com>
2018-01-30 14:25:49 +0100

    Inserimento nuovo database

--- SQL/urErasmus.sql
+++ SQL/urErasmus.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Creato il: Feb 02, 2017 alle 09:29
--- Versione del server: 10.1.19-MariaDB
--- Versione PHP: 7.0.13
+-- Host: localhost
+-- Creato il: Gen 30, 2018 alle 14:24
+-- Versione del server: 10.1.28-MariaDB
+-- Versione PHP: 7.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -20,7 +22,29 @@ SET time_zone = "+00:00";
 -- Database: `urErasmus`
 --
 
--- -------------------------------------------------------- 
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `admin`
+--
+
+CREATE TABLE `admin` (
+  `KEYUTENTE` int(11) NOT NULL,
+  `ID` varchar(32) DEFAULT NULL,
+  `EMAIL` varchar(40) DEFAULT NULL,
+  `PASS` varchar(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `admin`
+--
+
+INSERT INTO `admin` (`KEYUTENTE`, `ID`, `EMAIL`, `PASS`) VALUES
+(1, 'admin', 'admin@admin.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Struttura della tabella `annuncio`
 --
 
@@ -38,14 +62,14 @@ CREATE TABLE `annuncio` (
 --
 
 INSERT INTO `annuncio` (`KEYANNUNCIO`, `TITOLO`, `DESCRIZIONE`, `CONTATTO`, `DATADICARICAMENTO`, `KEYUTENTE`) VALUES
-(1, 'Ripetizioni Programmazione C', 'Dedico delle ore agli studenti in difficoltÃ  per il corso di programmazione in C', 'toremarone@gmail.com', '2017-01-31', 1),
-(2, 'Ripetizioni chimica organica', 'Ripetizioni per chimica organica dal lunedÃ¬ al venerdÃ¬ dalle 18:30 per info contattatemi', 'v.noviello@gmail.com', '2017-01-31', 3),
-(3, 'Lezioni di tutorato di Fisica Applicata', 'Offro lezioni di tutorato di Fisica Applicata a pagamento. Per info contattare il numero in basso.', '345 45 85 360', '2017-01-31', 7);
+(1, 'UniSa', 'Programmazione 1', 'professore@unisa.it', '2017-01-31', 1),
+(2, 'UGR', 'Programacion avanzada', 'profesor@ugr.es', '2017-01-31', 3),
+(3, 'University of Oxford', 'Computer science', 'professor@oxford.uk', '2017-01-31', 7);
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `corsi` Keyutente= 
+-- Struttura della tabella `corsi`
 --
 
 CREATE TABLE `corsi` (
@@ -54,7 +78,7 @@ CREATE TABLE `corsi` (
   `CATEGORIA` varchar(25) NOT NULL,
   `ANNOCORSO` varchar(25) DEFAULT NULL,
   `DATADICARICAMENTO` date NOT NULL,
-  `KEYUTENTE` int(11) DEFAULT NULL 
+  `KEYUTENTE` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -62,18 +86,20 @@ CREATE TABLE `corsi` (
 --
 
 INSERT INTO `corsi` (`KEYCORSI`, `NOME`, `CATEGORIA`, `ANNOCORSO`, `DATADICARICAMENTO`, `KEYUTENTE`) VALUES
-(1, 'Architettura degli elaboratori', 'Informatica', 'Primo anno, primo semestre', '2017-01-31', 1),
-(2, 'Algoritmi & Strutture Dati', 'Informatica', 'Secondo anno, secondo semestre', '2017-01-31', 1),
-(3, 'Analisi Matematica', 'Matematica', 'Primo anno, secondo semestre', '2017-01-31', 2),
-(4, 'Analisi Matematica informatica', 'Informatica', 'Primo anno, secondo semestre', '2017-01-31', 2),
-(7, 'Chimica Organica', 'Chimica', 'Secondo anno, primo semestre', '2017-01-31', 3),
-(8, 'Chimica Inorganica', 'Chimica', 'Terzo anno, secondo semestre', '2017-01-31', 3),
-(9, 'Biologia Molecolare', 'Biologia', 'Terzo anno, primo semestre', '2017-01-31', 4),
-(10, 'Ecologia Vegetale', 'Scienze Ambientali', 'Secondo anno, primo semestre', '2017-01-31', 5),
-(11, 'Ecologia Generale', 'Scienze Ambientali', 'Secondo anno, secondo semestre', '2017-01-31', 5),
-(12, 'Fisica Applicata', 'Fisica', 'Primo anno, secondo semestre', '2017-01-31', 7);
+(1, 'Architettura degli elaboratori', 'Informatica', 'Primo anno, primo semestr', '2017-01-31', 1),
+(2, 'Algoritmi & Strutture Dati', 'Informatica', 'Secondo anno, secondo sem', '2017-01-31', 1),
+(3, 'Analisi Matematica', 'Matematica', 'Primo anno, secondo semes', '2017-01-31', 2),
+(4, 'Analisi Matematica informatica', 'Informatica', 'Primo anno, secondo semes', '2017-01-31', 2),
+(7, 'Chimica Organica', 'Chimica', 'Secondo anno, primo semes', '2017-01-31', 3),
+(8, 'Chimica Inorganica', 'Chimica', 'Terzo anno, secondo semes', '2017-01-31', 3),
+(9, 'Biologia Molecolare', 'Biologia', 'Terzo anno, primo semestr', '2017-01-31', 4),
+(10, 'Ecologia Vegetale', 'Scienze Ambientali', 'Secondo anno, primo semes', '2017-01-31', 5),
+(11, 'Ecologia Generale', 'Scienze Ambientali', 'Secondo anno, secondo sem', '2017-01-31', 5),
+(12, 'Fisica Applicata', 'Fisica', 'Primo anno, secondo semes', '2017-01-31', 7);
 
 -- --------------------------------------------------------
+
+--
 -- Struttura della tabella `tag`
 --
 
@@ -216,26 +242,15 @@ INSERT INTO `utente` (`KEYUTENTE`, `ID`, `EMAIL`, `CITTA`, `PASS`) VALUES
 (9, 'Alex91msn', 'Alex@email.it', 'Pesciola', 'pasword');
 
 --
--- Struttura della tabella `admin`
---
-CREATE TABLE `admin` (
-`KEYUTENTE` int(11) NOT NULL,
-`ID` varchar(32) DEFAULT NULL,
-`EMAIL` varchar(40) DEFAULT NULL,
-`PASS` varchar(16) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- s
---
-INSERT INTO `admin` (`KEYUTENTE`, `ID`, `EMAIL`, `PASS`) VALUES
-(1, 'admin', 'admin@admin.com', 'admin');
-
-
-
---
 -- Indici per le tabelle scaricate
 --
+
+--
+-- Indici per le tabelle `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`KEYUTENTE`),
+  ADD UNIQUE KEY `EMAIL` (`EMAIL`,`ID`);
 
 --
 -- Indici per le tabelle `annuncio`
@@ -278,42 +293,41 @@ ALTER TABLE `tagpercorsi`
 ALTER TABLE `utente`
   ADD PRIMARY KEY (`KEYUTENTE`),
   ADD UNIQUE KEY `EMAIL` (`EMAIL`,`ID`);
-  
-  -- Indici per le tabelle `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`KEYUTENTE`),
-  ADD UNIQUE KEY `EMAIL` (`EMAIL`,`ID`);
 
 --
 -- AUTO_INCREMENT per le tabelle scaricate
 --
 
 --
+-- AUTO_INCREMENT per la tabella `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `KEYUTENTE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT per la tabella `annuncio`
 --
 ALTER TABLE `annuncio`
   MODIFY `KEYANNUNCIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT per la tabella `corsi`
 --
 ALTER TABLE `corsi`
-  MODIFY `KEYCORSI` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `KEYCORSI` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT per la tabella `tag`
 --
 ALTER TABLE `tag`
   MODIFY `KEYTAG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
 --
 -- AUTO_INCREMENT per la tabella `utente`
 --
 ALTER TABLE `utente`
   MODIFY `KEYUTENTE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT per la tabella `admin`
---
-ALTER TABLE `admin`
-  MODIFY `KEYUTENTE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- Limiti per le tabelle scaricate
 --
@@ -336,15 +350,8 @@ ALTER TABLE `corsi`
 ALTER TABLE `tagperannuncio`
   ADD CONSTRAINT `tagperannuncio_ibfk_1` FOREIGN KEY (`KEYTAG`) REFERENCES `tag` (`KEYTAG`),
   ADD CONSTRAINT `tagperannuncio_ibfk_2` FOREIGN KEY (`KEYANNUNCIO`) REFERENCES `annuncio` (`KEYANNUNCIO`);
-
---
--- Limiti per la tabella `tagoercorsi`
---
-ALTER TABLE `tagpercorsi`
-  ADD CONSTRAINT `tagpercorsi_ibfk_1` FOREIGN KEY (`KEYTAG`) REFERENCES `tag` (`KEYTAG`),
-  ADD CONSTRAINT `tagpercorsi_ibfk_2` FOREIGN KEY (`KEYCORSI`) REFERENCES `corsi` (`KEYCORSI`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
