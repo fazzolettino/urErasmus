eec278d7c884fa97a6bfee9ca6fe3e0bd055574c
Alex <alex91msn@gmail.com>
2018-02-15 17:48:11 +0100

    aggiunto getannunciadmin

---
+++ core/control/getAnnunciAdmin.php
@@ -0,0 +1,25 @@
+<?php
+include_once MODEL_DIR . "AnnuncioManager.php";
+include_once MODEL_DIR ."UtenteManager.php";
+
+
+
+    
+
+    $manager = new AnnuncioManager();
+
+    $annunci = $manager->getAllAnnunci();
+
+
+    include_once VIEW_DIR . "AdminClasses.php";
+
+
+
+function testInput($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
