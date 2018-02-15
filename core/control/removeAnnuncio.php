7a799d70e13201e1f53b281b8789d87533469ad0
Alex <alex91msn@gmail.com>
2018-02-15 16:32:41 +0100

    aggiunta funzione, ancora da implementare

---
+++ core/control/removeAnnuncio.php
@@ -0,0 +1,28 @@
+<?php
+include_once MODEL_DIR . "AnnuncioManager.php";
+include_once MODEL_DIR ."UtenteManager.php";
+
+
+
+
+
+$manager = new AnnuncioManager();
+if (isset($_GET['keyToDelete'])){
+    $deleteKey = $_GET['keyToDelete'];
+
+    
+    $manager->cancellaAnnuncio($deleteKey);
+
+    $_SESSION['toast-type'] = "success";
+    $_SESSION['toast-message'] = "Deleted correctly";
+    header ("location: ". DOMINIO_SITO ."/AdminClasses");
+
+} else {
+    echo "Errore Rimozione";
+}
+
+
+
+
+
+?>
